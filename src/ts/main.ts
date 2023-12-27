3508772f1cd196713ad152587caf6bc43420734b
Nicke Bergman <nickenickebergman@gmail.com>
2023-12-27 14:02:43 +0100

    :sparkles: Added classes and initial styling
    Co-authored-by: LaNtZ445 <LaNtZ445@users.noreply.github.com>
    Co-authored-by: Hanneslol <hannes@hanneswallin.com>
    Co-authored-by: 2102kh <2102kh@users.noreply.github.com>

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,35 @@
 import "./../scss/style.scss";
+import { Product } from "./services/Product";
+
+const p1: Product = new Product(
+  1,
+  "Tiger",
+  5,
+  0.1,
+  "cute Tiger",
+  0,
+  "src/img/tiger.jpg",
+  "Indien"
+);
+const p2: Product = new Product(
+  1,
+  "Lejon",
+  4,
+  0.2,
+  "cute Lejon",
+  0,
+  "src/img/lejon.jpg",
+  "Kenya"
+);
+
+const product: Product[] = [p1, p2];
+console.log(product);
+
+/* public id: number,
+public name: string,
+public age: number,
+public price: number,
+public description: string,
+public quantity: number,
+public image: string,
+public country: string */
