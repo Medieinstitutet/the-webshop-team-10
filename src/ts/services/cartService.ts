6aaabddc10b948eb4899134c6aeda2f23bea03d2
Nicke Bergman <nickenickebergman@gmail.com>
2023-12-28 11:19:56 +0100

    :art: Finalized function to add to cart
    Co-authored-by: LaNtZ445 <LaNtZ445@users.noreply.github.com>
    Co-authored-by: Hanneslol <hannes@hanneswallin.com>
    Co-authored-by: 2102kh <2102kh@users.noreply.github.com>

--- src/ts/services/cartService.ts
+++ src/ts/services/cartService.ts
@@ -5,7 +5,15 @@ import { p1 } from "../main";
 // import { p2 } from "../main";
 
 const addToCart = (product: Product, quantity: number) => {
-  cart.push(new CartItem(product, quantity));
+  let alreadyInCart = false;
+
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].product.id === product.id) {
+      cart[i].quantity += quantity;
+      alreadyInCart = true;
+    }
+  }
+  if (alreadyInCart === false) cart.push(new CartItem(product, quantity));
   console.log(cart);
 };
 
