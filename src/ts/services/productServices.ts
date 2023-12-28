f2c66c4a928c986c050c271156d42ba67a5c8976
Nicke Bergman <nickenickebergman@gmail.com>
2023-12-28 16:00:48 +0100

    :art: Improved documentation
    Co-authored-by: LaNtZ445 <LaNtZ445@users.noreply.github.com>
    Co-authored-by: 2102kh <2102kh@users.noreply.github.com>
    Co-authored-by: Hanneslol <hannes@hanneswallin.com>

--- src/ts/services/productServices.ts
+++ src/ts/services/productServices.ts
@@ -24,32 +24,39 @@ export const p2: Product = new Product(
 export const products: Product[] = [p1, p2];
 
 const productsContainer = document.getElementById("productsContainer");
+//Fuktion för att skapa html för produkter.
 const createProductsHtml = () => {
+  //Loopar igenom hela products och genererar html.
   for (let i = 0; i < products.length; i++) {
+    //Skapar element
     const productCard = document.createElement("div");
     const productCardImage = document.createElement("img");
     const productCardName = document.createElement("h3");
     const productCardPrice = document.createElement("span");
     const productCardButton = document.createElement("button");
 
+    //Tilldelar klasser
     productCard.className = "product-card";
     productCardImage.className = "product-card__image";
     productCardName.className = "product-card__name";
     productCardPrice.className = "product-card__price";
     productCardButton.className = "product-card__button";
 
+    //Lägger till produktinformation
     productCardImage.src = products[i].image;
     productCardImage.alt = products[i].name;
     productCardName.innerHTML = products[i].name;
     productCardPrice.innerHTML = products[i].price.toString() + "&#x20bf;";
     productCardButton.innerHTML = "Lägg till i varukorg";
 
+    //Placerar elementen
     productsContainer?.appendChild(productCard);
     productCard.appendChild(productCardImage);
     productCard.appendChild(productCardName);
     productCard.appendChild(productCardPrice);
     productCard.appendChild(productCardButton);
 
+    //Funktionalitet för knapp
     productCardButton.addEventListener("click", () => {
       addToCart(products[i], 1);
     });
