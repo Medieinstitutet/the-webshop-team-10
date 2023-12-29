939a2a96015577853fbaec0d589a6a0929ef0a1c
LaNtZ445 <jacoblimparlantz@gmail.com>
2023-12-29 13:38:06 +0100

    uppdaterade s det finns kommentarer för varje steg

--- src/ts/services/navbarServices.ts
+++ src/ts/services/navbarServices.ts
@@ -1,46 +1,86 @@
-//Skapa Navbar genom att genererar de med ts
+// Skapar header elementet
 const header = document.createElement('header');
 header.className = 'header';
 header.id = 'header';
 
+// Skapar navbar elementet
 const navbar = document.createElement('nav');
 navbar.className = 'navbar';
 
+// Skapar logo bilden
 const navbarLogoImage = document.createElement('img');
 navbarLogoImage.className = 'navbar__logo-image';
-navbarLogoImage.src = '#'; 
+navbarLogoImage.src = '#';
 navbarLogoImage.alt = 'Logo';
 
+// Skapar länk till logans namn (hemsidans namn)
 const navbarLogoName = document.createElement('a');
 navbarLogoName.className = 'navbar__logo-name';
-navbarLogoName.href = '#'; 
-navbarLogoName.innerHTML = 'Webshop Name...nått coolt';
+navbarLogoName.href = '#';
+navbarLogoName.innerHTML = 'Webbshop Namn...något coolt';
 
+// Skapa ul elementet
 const navbarUl = document.createElement('ul');
 navbarUl.className = 'navbar__ul';
 
-const navbarLi = document.createElement('li');
-navbarLi.className = 'navbar__li';
+// Skapar Home länk och html på navbaren
+const navbarLiHome = document.createElement('li');
+navbarLiHome.className = 'navbar__li';
 
 const navbarLinkHome = document.createElement('a');
 navbarLinkHome.href = '#';
-navbarLinkHome.innerHTML = 'Home';
+navbarLinkHome.innerHTML = 'Hem';
+navbarLiHome.appendChild(navbarLinkHome);
+navbarUl.appendChild(navbarLiHome);
 
+// Skapar Produkter länk och html på navbaren
+const navbarLiProduct = document.createElement('li');
+navbarLiProduct.className = 'navbar__li';
+
+const navbarLinkProduct = document.createElement('a');
+navbarLinkProduct.href = '#';
+navbarLinkProduct.innerHTML = 'Produkter';
+navbarLiProduct.appendChild(navbarLinkProduct);
+navbarUl.appendChild(navbarLiProduct);
+
+// Skapar Kassa länk och html på navbaren
+const navbarLiCart = document.createElement('li');
+navbarLiCart.className = 'navbar__li';
+
+const navbarLinkCart = document.createElement('a');
+navbarLinkCart.href = '#';
+navbarLinkCart.innerHTML = 'Kassa';
+navbarLiCart.appendChild(navbarLinkCart);
+navbarUl.appendChild(navbarLiCart);
+
+// Skapar Om Oss länk och html på navbaren
+const navbarLiAboutUs = document.createElement('li');
+navbarLiAboutUs.className = 'navbar__li';
+
+const navbarLinkAboutUs = document.createElement('a');
+navbarLinkAboutUs.href = '#';
+navbarLinkAboutUs.innerHTML = 'Om Oss';
+navbarLiAboutUs.appendChild(navbarLinkAboutUs);
+navbarUl.appendChild(navbarLiAboutUs);
+
+// Skapar knapp för varukorg
 const cartButton = document.createElement('button');
 cartButton.id = 'cartButton';
 cartButton.innerHTML = 'Varukorg';
 
+// Skapar hamburgermenu
 const hamburgerMenu = document.createElement('div');
 hamburgerMenu.className = 'hamburger-menu';
 
-navbarLi.appendChild(navbarLinkHome); 
-navbarUl.appendChild(navbarLi);
-navbarLogoName.appendChild(navbarLogoImage);
+// Lägg till element för att bygga navbar
+navbar.appendChild(navbarLogoImage);
 navbar.appendChild(navbarLogoName);
 navbar.appendChild(navbarUl);
+navbar.appendChild(cartButton);
+navbar.appendChild(hamburgerMenu);
+
+// Lägg till navbar i header och header i bodyn
 header.appendChild(navbar);
-
-
 document.body.appendChild(header);
 
 
@@ -69,15 +109,4 @@ document.body.appendChild(header);
 </nav>
 </header>*/
 
-/*vet inte om man ska använda dom här eller om de bara funkar med home*/
-/*const navbarLinkProduct = document.createElement('a');
-navbarLinkProduct.href = '#';
-navbarLinkProduct.innerHTML = 'Produkter';
 
-const navbarLinkCart = document.createElement('a');
-navbarLinkCart.href = '#';
-navbarLinkCart.innerHTML = 'Kassa';
-
-const navbarLinkAboutUs = document.createElement('a');
-navbarLinkAboutUs.href = '#';
-navbarLinkAboutUs.innerHTML = 'Om Oss';*/
